32169ac7e86d929a86f10ba0d479be21b6e7cfdc
tavietdat123 <108579970+tavietdat123@users.noreply.github.com>
2022-11-10 13:33:53 +0700

    main

--- content/src/header.js
+++ content/src/header.js
@@ -3,9 +3,11 @@ const nav = document.querySelector('.nav')
 const nav_link = document.querySelectorAll('.nav_link')
 const momondo = document.querySelector('.header')
 const content = document.querySelector('.container_content')
+const content1 = document.querySelector('.content')
 const footer = document.querySelector('.footer')
-let control = false
+const footer1 = document.querySelector('.container_footer')
 
+let control = false
 console.dir(nav)
 btnHeader.onclick = ()=>{
     if(control){
@@ -13,11 +15,11 @@ btnHeader.onclick = ()=>{
         nav_link.forEach(el=>{
         el.children[1].style.display = 'block'
         }) 
-        
-
     momondo.style.padding = `0px 18px 0px 256px` 
     content.style.padding = `62px 0 0 240px`
     footer.style.padding = `40px 0 0 240px`
+    content1.style.padding ="36px 30px "
+    footer1.style.padding = "0 30px"
 
 
         control= false
@@ -26,18 +28,13 @@ btnHeader.onclick = ()=>{
         nav_link.forEach(el=>{
         el.children[1].style.display = 'none'
         }) 
-        
         content.style.padding = `62px 0 0 64px`
+        content1.style.padding ="36px 85px "
         momondo.style.padding = `0px 18px 0px 80px` 
         footer.style.padding = `40px 0 0 64px`
+        footer1.style.padding = "0 85px"
         control= true
-        
-  
-        
-        
       } 
-    
-    
     }
     
     
